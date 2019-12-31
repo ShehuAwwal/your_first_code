5075046e7d6f6fb7e70c9e949d803f0be3e3b454
Shehu Awwal <awwalshehu@gmail.com>
2019-12-31 12:47:31 +0100

    Solved

--- main.c
+++ main.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-int main()
-{
-  int j;
-  for (j = 0; j < 32; j++)
-  {
+int main(){
+    int j = 0;
+    while (j < 32){
     printf("Hello Holberton\n");
-  }
+    j++;
+    }
   return 0;
 }
